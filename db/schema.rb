a5f90eb4677b7d90402333947914fb609dbe5555
DoHungThinh <12520919@gm.uit.edu.vn>
2016-04-13 15:00:21 +0700

    Add foreignkey for Product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160413075448) do
+ActiveRecord::Schema.define(version: 20160413075844) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20160413075448) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
+
+  add_index "products", ["category_id"], name: "index_products_on_category_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
