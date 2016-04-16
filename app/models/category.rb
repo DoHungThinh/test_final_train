a71b2636d30db0441433045deab2e28cffee97c1
DoHungThinh <12520919@gm.uit.edu.vn>
2016-04-16 16:14:53 +0700

    Edit show category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 has_many :products
-has_attached_file :image, styles: { medium: "350x400#", thumb: "100x100>" }
+has_attached_file :image, styles: { medium: "350x400", large: "350x400#" }
 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 paginates_per 12
 end
